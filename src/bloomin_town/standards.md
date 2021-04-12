ce256fd934ebefa6f246c41f956b76f3851ee765
Chris Hale <hale9644@pacificu.edu>
2021-04-12 13:52:40 -0700

    #21 create initial bloomin town datapack

---
+++ src/bloomin_town/standards.md
@@ -0,0 +1,13 @@
+This section uses [mcdatapacks](https://mc-datapacks.github.io/en/index.html) standards as a checklist
+
+This datapack is a NiT addon.
+
+- [ ] Datapack Advancement: Added advancement as per the standard. Tests ensure these are used properly.
+- [ ] Datapack Uninstallation:
+- [ ] Common Trait Convention:
+- [ ] Custom Model ID: NOT NEEDED
+- [ ] Global Ignoring Tag:
+- [ ] Global Durability: NOT NEEDED
+- [ ] Namespace Convention: using bloomin_town namespac
+- [ ] Shulker Box Loot Table:  NOT NEEDED
+- [ ] Reloading Message:
---
+++ test/bloomin_town.spec.js
@@ -0,0 +1,48 @@
+const { should } = require('chai');
+should();
+const { packTests } = require('./pack_common.js');
+// const { startServer, endServer } = require('./server.js');
+
+describe('Bloomin Town', function () {
+  // let minebot;
+  // this.beforeAll(function(done) {
+  //   this.timeout(120000);
+  //   startServer('snapshot').then((bot) => {
+  //     minebot = bot;
+  //     done();
+  //   }).catch(error => {
+  //     done(error);
+  //   });
+  // });
+
+  // this.afterAll(function(done) {
+  //   this.timeout(5000);
+  //   endServer(minebot).then(() => {
+  //     done();
+  //   }).catch(error => {
+  //     done(error);
+  //   });
+  // });
+
+  describe('installed datapacks advancements', function () {
+    packTests('bloomin_town');
+    it('should have bloomin_town', function () {
+      const json = require('../build/bloomin_town/data/bloomin_town/advancements/bloomin_town.json');
+      json.should.have.deep.property('display', {
+        title: "Bloomin' Town",
+        description: 'Spring time in New in Town!',
+        icon: {
+          item: 'minecraft:sweet_berries'
+        },
+        announce_to_chat: false,
+        show_toast: false
+      });
+      json.should.have.property('parent', 'global:jolly_packs');
+      json.should.have.deep.property('criteria', {
+        trigger: {
+          trigger: 'minecraft:tick'
+        }
+      });
+    });
+  });
+});
